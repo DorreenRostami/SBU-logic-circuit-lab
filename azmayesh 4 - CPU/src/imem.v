f05913d4cc13004d8aeed4c16feeacc4378bc85e
DorreenRostami <46872718+DorreenRostami@users.noreply.github.com>
2020-11-06 17:17:36 +0330

    Add files via upload

---
+++ azmayesh 4 - CPU/src/imem.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+module imem(
+	 input [7:0] a,
+	 output [15:0] rd
+	 );
+
+	reg [15:0] RAM[255:0];
+
+	initial 
+		$readmemb("E:\\AZ\\CPU\\memfile.dat",RAM);
+
+	assign rd = RAM[a[7:2]];
+
+endmodule
